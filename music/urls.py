e7793dca40ddfafd62a6540f87c89fcce0e2788e
ivvraghavendra <ivvraghavendra1999@gmail.com>
2018-10-24 23:49:05 +0530

    #5

--- music/urls.py
+++ music/urls.py
@@ -3,16 +3,16 @@ from django.urls import path, include
 from django.views.generic import TemplateView, ListView
 from . import views
 from django.conf.urls import url
-from music.views import EnglishAlbumView
+# from music.views import EnglishAlbumView
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='home.html'),name='home'),
+    # path('',TemplateView.as_view(template_name='home.html'),name='home'),
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('main/',TemplateView.as_view(template_name='main.html'),name='main'),
     url(r'^telugu/(?P<album_id>[0-9]+)/$',TemplateView.as_view(template_name='telugu.html'),name='telugu'),
-    path('english/(?P<album_id>[0-9]+)/$',EnglishAlbumView.as_view(),name='english'),
+    url(r'^english/(?P<album_id>[0-9]+)/$', views.index,name='english'),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^$', views.index, name='index'),
 ]
--- music/views.py
+++ music/views.py
@@ -3,14 +3,10 @@ from .models import Album
 from django.shortcuts import render
 from django.views.generic import ListView
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums' : all_albums }
-    return render(request, 'music/english.html', context)
+def index(request,pk):
+    all_albums = Album.objects.get(pk=pk)
+    context = {'album_list' : all_albums }
+    return render(request, 'english.html', context)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details of the Album id: " + str(album_id) + "</h2>")
-
-class EnglishAlbumView(ListView):
-    model = Album.objects.all()
-    template_name = "english.html"
